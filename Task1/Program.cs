747dab0ece2d07d55948d7b08e2d4f551a779e30
IGOR <sahmen@mail.ru>
2022-12-26 11:21:41 +0300

    DZ 1

---
+++ Task1/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Clear();
+Console.Write("Введите начальное число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите конечное число: ");
+int m = Convert.ToInt32(Console.ReadLine());
+
+void sumN (int n, int m, int sum)
+{
+    if (n > m) 
+    {
+        Console.WriteLine($"Сумма натуральных элементов в промежутке от n до m: {sum}"); 
+        return;
+    }
+    sum = sum + (n++);
+    sumN(n, m, sum);
+}
+sumN(n, m, 0);
